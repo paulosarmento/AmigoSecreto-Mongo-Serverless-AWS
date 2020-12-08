86820c2bc245ba05571b7d07e2348b4ac45834ca
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2020-12-08 09:59:26 -0300

    Rota post dos participantes

--- src/handlers/participants.js
+++ src/handlers/participants.js
@@ -1,7 +1,43 @@
+const { v4: uuidv4 } = require("uuid");
+require("../resources/db/connection")();
+
+const SecretModel = require("../resources/db/models/Secret");
+
 module.exports.create = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
+  const { id: secretId } = event.pathParameters;
+  const { name, email } = JSON.parse(event.body);
+  const externalId = uuidv4();
+
   try {
+    const result = await SecretModel.updateOne(
+      {
+        externalId: secretId,
+        "participants.email": { $ne: email },
+      },
+      {
+        $push: {
+          participants: {
+            externalId,
+            name,
+            email,
+          },
+        },
+      }
+    );
+
+    if (!result.nModified) {
+      throw new Error();
+    }
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        success: true,
+        id: externalId,
+      }),
+    };
   } catch (error) {
     return {
       statusCode: 500,
